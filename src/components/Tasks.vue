93c7cf0f2206a7deaed5072d6d1121f8dcfe1838
Sergey Kharitonov <serhento@gmail.com>
2021-02-22 18:43:05 +0300

    init

---
+++ src/components/Tasks.vue
@@ -0,0 +1,241 @@
+<template>
+    <div class="tasks">
+        <h2 v-if="!LISTS[SELECTED_TASK].img" class="tasks__title" :class="['badge__'+LISTS[SELECTED_TASK].color.name]">
+            {{LISTS[SELECTED_TASK].name}}
+            <img @click="changeTaskTitle(LISTS[SELECTED_TASK].name, LISTS[SELECTED_TASK].id)" src="@/assets/img/edit.svg" alt="Edit">
+        </h2>
+
+        <div v-if="!LISTS[SELECTED_TASK].img" class="tasks__items">
+            <h2 v-if="!LISTS[SELECTED_TASK].tasks.length">Задачи отсутствуют</h2>
+            <div v-for="task in TASKS" :key="task.id">
+                <div class="tasks__items-row" v-if="task.listId === SELECTED_TASK+1">
+                    <div class="tasks__items-row-item">
+                        <div class="tasks__items-row-item-checkbox">
+                            <input @click="onCompleted(task.id, task.completed)" :id="[task.id]" type="checkbox" :checked="task.completed"/>
+                            <label :for="[task.id]">
+                                <svg width="12" height="10" viewBox="0 0 12 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M4.33342 9.16649L0.166748 4.99982L1.34175 3.82482L4.33342 6.80815L10.6584 0.483154L11.8334 1.66649L4.33342 9.16649Z" fill="white"/>
+                                </svg>
+                            </label>
+                        </div>
+                        <p>{{task.text}}</p>
+                    </div>
+                    <div class="tasks__items-row-tools">
+                        <img @click="editTaskName(task.id, task.text)" src="@/assets/img/edit.svg" alt="">
+                        <img @click="deleteTask(task.id)" src="@/assets/img/remove.svg" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <AddTaskForm
+                v-if="!LISTS[SELECTED_TASK].img"
+                :listId="this.LISTS[SELECTED_TASK].id"
+            />
+        </div>
+        <p id="prolog" v-if="LISTS[SELECTED_TASK].img">
+            На просторах youtube есть урок по созданию этого приложения с помощью библиотеки React JS.
+            Я взял этот урок, как образец, и написал приложение с помощью фреймворка Vue JS. Стили практически не менял,
+            т.к для меня, в первую очередь, было важным продемонстрировать навыки работы с Vue JS.
+        </p>
+    </div>
+</template>
+
+<script>
+    import AddTaskForm from './AddTaskForm';
+    import {mapActions, mapGetters} from 'vuex';
+    export default {
+        name: "Tasks",
+        components:{
+            AddTaskForm
+        },
+        computed:{
+            ...mapGetters([
+                'LISTS', 'SELECTED_TASK', 'TASKS'
+            ])
+        },
+        methods:{
+            ...mapActions([
+                'GET_LISTS_FROM_API',
+                'EDIT_TASK_TITLE',
+                'GET_TASKS_FROM_API',
+                'ON_TASK_COMPLETED',
+                'EDIT_TASK_NAME',
+                'DELETE_TASK_FROM_TASKS'
+            ]),
+            changeTaskTitle(name, id){
+                const newTitle = window.prompt('Название списка', name);
+                if (newTitle){
+                    const newData = {"dataTitle": newTitle, "dataId": id}
+                    this.EDIT_TASK_TITLE(newData)
+                }
+            },
+            onCompleted(id, taskCompleted){
+                const newData = {"id": id, "completed": taskCompleted};
+                this.ON_TASK_COMPLETED(newData);
+                setTimeout(this.GET_TASKS_FROM_API, 100);
+            },
+            editTaskName(id, text){
+                const newTitle = window.prompt('Название задачи', text);
+                if (newTitle){
+                    const newData = {"text": newTitle, "id": id}
+                    this.EDIT_TASK_NAME(newData)
+                }
+            },
+            deleteTask(id){
+                console.log(id)
+                if (window.confirm('Вы действительно хотите удалить задачу?')){
+                    this.DELETE_TASK_FROM_TASKS(id);
+                    setTimeout(this.GET_TASKS_FROM_API, 100);
+                    setTimeout(this.GET_LISTS_FROM_API, 100);
+                }
+            }
+        },
+        mounted() {
+            this.GET_TASKS_FROM_API()
+                .then((res)=>{
+                    if(res.data){
+                        console.log('Data arrived')
+                    }
+                });
+        }
+    }
+</script>
+
+<style lang="scss">
+    .tasks{
+
+        #prolog{
+            font-size: 18px;
+            font-family: Montserrat;
+            line-height: 25px;
+        }
+
+        &__title{
+            font-family: Montserrat;
+            font-weight: bold;
+            font-size: 32px;
+            line-height: 39px;
+            color: #64C4ED;
+            padding-bottom: 20px;
+            border-bottom: 1px solid #F2F2F2;
+            flex: 1;
+            text-overflow: ellipsis;
+            overflow: hidden;
+            white-space: nowrap;
+
+            &:hover{
+                img{
+                    opacity: 0.2;
+                }
+            }
+
+            img{
+                opacity: 0;
+                cursor: pointer;
+                margin-left: 5px;
+
+                &:hover{
+                    opacity: 0.8;
+                }
+            }
+        }
+
+        &__items{
+            margin-top: 30px;
+
+            h2{
+                font-family: Montserrat;
+                font-weight: bold;
+                font-size: 32px;
+                color: #C9D1D3;
+            }
+
+            &-row{
+                display: flex;
+                align-items: start;
+                margin-bottom: 15px;
+                justify-content: space-between;
+
+                &:hover{
+                    img{
+                        opacity: 0.2;
+                    }
+                }
+
+                &-item{
+                    display: flex;
+                    align-items: center;
+                    cursor: pointer;
+
+                    p{
+                        margin-left: 15px;
+                    }
+
+                    &-checkbox{
+                        &:hover{
+                            label{
+                                background-color: #f2f2f2;
+                                border-color: #f2f2f2;
+
+                                svg{
+                                    opacity: 1;
+                                    path{
+                                        fill: #b2b2b2;
+                                    }
+                                }
+                            }
+                        }
+
+                        input{
+                            display: none;
+                        }
+
+                        input:checked + label{
+                            background: #2c8498;
+                            border-color: #2c8498;
+
+                            svg{
+                                opacity: 1;
+                                path{
+                                    fill: #fff;
+                                }
+                            }
+                        }
+
+                        label{
+                            display: flex;
+                            justify-content: center;
+                            align-items: center;
+                            cursor: pointer;
+                            border: 2px solid #e8e8e8;
+                            width: 20px;
+                            height: 20px;
+                            border-radius: 30px;
+
+                            svg{
+                                opacity: 0;
+                            }
+                        }
+                    }
+                }
+
+                &-tools{
+                    display: flex;
+                    align-items: center;
+
+                    img{
+                        height: 13px;
+                        width: 13px;
+                        margin-left: 5px;
+                        opacity: 0;
+                        cursor: pointer;
+
+                        &:hover{
+                            opacity: 0.8;
+                        }
+                    }
+                }
+            }
+        }
+    }
+</style>
---
+++ src/vuex/store.js
@@ -0,0 +1,203 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
+
+Vue.use(Vuex);
+
+let store = new Vuex.Store({
+    state: {
+        lists: [],
+        tasks: [],
+        colors: [],
+        selectedTask: 0
+    },
+    actions: {
+        GET_LISTS_FROM_API({commit}){
+            return axios('http://localhost:3000/lists?_expand=color&_embed=tasks', {
+                method: "GET"
+            })
+                .then((lists)=>{
+                    commit('SET_LISTS_TO_STATE', lists.data);
+                    return lists;
+                })
+                .catch((error)=>{
+                    console.log(error);
+                    return error;
+                })
+        },
+        ADD_TASK_TO_LIST({commit}, task){
+            //commit('ADD_TASK_TO_STATE',task);
+            axios.post('http://localhost:3000/lists/', task)
+                .then(()=>{
+                    commit('ADD_TASK_TO_STATE',task);
+                })
+                .catch((error)=>{
+                    console.log(error);
+                    return error;
+                })
+
+        },
+        DELETE_LIST_FROM_LISTS({commit}, id){
+            axios.delete(`http://localhost:3000/lists/${id}`)
+                .then(()=>{
+                    commit('DELETE_LIST_FROM_STATE',id);
+                })
+                .catch((error)=>{
+                    console.log(error);
+                    return error;
+                })
+        },
+        GET_TASKS_FROM_API({commit}){
+            return axios('http://localhost:3000/tasks', {
+                method: "GET"
+            })
+                .then((tasks)=>{
+                    commit('SET_TASKS_TO_STATE', tasks.data);
+                    return tasks;
+                })
+                .catch((error)=>{
+                    console.log(error);
+                    return error;
+                })
+        },
+        GET_COLORS_FROM_API({commit}){
+            return axios('http://localhost:3000/colors', {
+                method: "GET"
+            })
+                .then((colors)=>{
+                    commit('SET_COLORS_TO_STATE', colors.data);
+                    return colors;
+                })
+                .catch((error)=>{
+                    console.log(error);
+                    return error;
+                })
+        },
+        SET_ACTIVE_TASK({commit}, id){
+            commit('SET_SELECTED_TASK', id)
+        },
+        EDIT_TASK_TITLE({commit}, data){
+            axios.patch(`http://localhost:3000/lists/${data.dataId}`, {
+                name: data.dataTitle
+            })
+                .then(()=>{
+                    commit('CHANGE_TASK_TITLE',data);
+                })
+                .catch(()=>{
+                    alert('Не удалось обновить название списка')
+                })
+        },
+        EDIT_TASK_NAME({commit}, data){
+            axios.patch(`http://localhost:3000/tasks/${data.id}`, {
+                text: data.text
+            })
+                .then(()=>{
+                    commit('CHANGE_TASK_NAME',data);
+                })
+                .catch(()=>{
+                    alert('Не удалось обновить')
+                })
+        },
+        DELETE_TASK_FROM_TASKS({commit}, id){
+            axios.delete(`http://localhost:3000/tasks/${id}`)
+                .then(()=>{
+                    commit('DELETE_TASK_FROM_STATE',id);
+                })
+                .catch((error)=>{
+                    console.log(error);
+                    return error;
+                })
+        },
+        ON_TASK_COMPLETED({commit}, data){
+            axios.patch(`http://localhost:3000/tasks/${data.id}`, {
+                completed: !data.completed
+            })
+                .then(()=>{
+                    commit('SET_TASK_COMPLETED', data);
+                })
+                .catch((error)=>{
+                    console.log(error)
+                })
+        },
+        ADD_TASK_TO_TASKS({commit}, task){
+            axios.post('http://localhost:3000/tasks/', task)
+                .then(()=>{
+                    commit('ADD_TASK_TO_TASKS_STATE',task);
+                })
+                .catch((error)=>{
+                    console.log(error);
+                    return error;
+                })
+
+        }
+    },
+    mutations: {
+        SET_LISTS_TO_STATE:(state, lists)=>{
+            state.lists = lists;
+        },
+        ADD_TASK_TO_STATE:(state, task)=>{
+            state.lists.push(task)
+        },
+        DELETE_LIST_FROM_STATE:(state, id)=>{
+            state.lists.splice(id, 1)
+        },
+        SET_TASKS_TO_STATE:(state, tasks)=>{
+            state.tasks = tasks;
+        },
+        SET_COLORS_TO_STATE:(state, colors)=>{
+            state.colors = colors;
+        },
+        SET_SELECTED_TASK:(state, id)=>{
+            state.selectedTask = id
+        },
+        CHANGE_TASK_TITLE:(state, data)=>{
+            const newList = state.lists.map(item=>{
+                if (item.id === data.dataId){
+                    item.name = data.dataTitle
+                }
+                return item
+            });
+            state.lists = newList;
+        },
+        CHANGE_TASK_NAME:(state, data)=>{
+            const newList = state.tasks.map(item=>{
+                if (item.id === data.id){
+                    item.text = data.text
+                }
+                return item
+            });
+            state.tasks = newList;
+        },
+        DELETE_TASK_FROM_STATE:(state, id)=>{
+            state.tasks.splice(id, 1)
+        },
+        SET_TASK_COMPLETED:(state, data)=>{
+            const newTasks = state.tasks.map((item)=>{
+                if (item.id === data.id){
+                    item.completed = !item.completed
+                }
+                return item
+            });
+            state.tasks = newTasks;
+        },
+        ADD_TASK_TO_TASKS_STATE:(state, task)=>{
+            state.tasks.push(task)
+        }
+    },
+    getters: {
+        LISTS(state){
+            return state.lists
+        },
+        TASKS(state){
+            return state.tasks
+        },
+        COLORS(state){
+            return state.colors
+        },
+        SELECTED_TASK(state){
+            return state.selectedTask
+        }
+    }
+});
+
+export default store;
